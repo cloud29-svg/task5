62ccba0d21b0c950a48ac6f5fc751c84721e351f
Inya Precious <chijiokeprecious79@gmail.com>
2025-07-14 00:33:14 +0100

    My first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+// Step 1: Declare variable
+const internName ="Inya Precious";
+let taskOne = 10;
+var taskTwo = 8;
+const taskThree = 6;
+
+
+
+// Step 2: Calculate total and average
+const totalScore = taskOne + taskTwo + taskThree;
+const averageScore = totalScore / 3;
+const roundedAverage = 
+averageScore. toFixed(2);
+
+// Step 3: Determine Grade
+let grade;
+if (averageScore >= 9) {
+    grade = 'A';
+} else if (averageScore >= 8) {
+    grade = 'B';
+} else if (averageScore >= 7) {
+    grade = 'C';
+} else if (averageScore >= 6) {
+    grade = 'D';
+} else {
+    grade = 'E';
+}
+
+// Step 4:Display Output
+console.log(` Student Name: ${internName}
+    Task Scores: ${taskOne}, ${taskTwo}, ${taskThree}
+    Average Score: ${roundedAverage}
+    Final Grade: ${grade}
+
+    `);
+
+    // Step 5: Feedback Message
+    let feedback;
+    switch(grade) {
+        case 'A':
+            feedback = "Excellent performance!";
+            break;
+            case 'B':
+                feedback = "Great job! keep amining high.";
+                case 'C':
+                    feedback ="Good effort.Try to improve";
+                    break;
+                    case 'D':
+                        feedback = "You are close. keep working at it.";
+                        break;
+                        case 'E':
+                            feedback = "Don't give up. you can do better next time.";
+                            break;
+                            default:
+                                feedback = "No feedback available";
+    }
+    console.log(`feedback:  ${feedback}`);
+    
+
+
+
+ 
+
